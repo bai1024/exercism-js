ad8d7787c295c76ea641d387656920faa0e3aa55
BaiYan <dingdingbai0424@gmail.com>
2018-05-23 10:41:37 +0800

    refactor hello-world

--- javascript/binary/binary.js
+++ javascript/binary/binary.js
@@ -3,8 +3,8 @@ class Binary {
     this.num = /[^01]/.test(num) ? "0": num
   }
 
-  toDecimal(){
-    return this.num.split("").reverse().reduce((x, y, i)=>{
+  toDecimal() {
+    return this.num.split("").reverse().reduce((x, y, i) =>{
       return (y === "1") ? x + Math.pow(2, i) : x;
     }, 0);
   }
