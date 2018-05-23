f1144d6aaea04c95c38a21ba1b67dbed87329315
BaiYan <dingdingbai0424@gmail.com>
2018-05-23 11:34:12 +0800

    refactor code

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -1,15 +1,14 @@
-
 var ArmstrongNumber = {
-	validate:(num) => {
-        let len = num.toString().split('').length
-        let sum = num
-            .toString()
-            .split('')
-            .map(e => Math.pow(e, len))
-            .reduce(((sum, e) => sum += e))
-        if (sum == num) return true
-        else return false
-    }
+  validate:(num) => {
+    let len = num.toString().split('').length
+    let sum = num
+              .toString()
+              .split('')
+              .map(e => Math.pow(e, len))
+              .reduce(((sum, e) => sum += e))
+    if (sum == num) return true
+    else return false
+  }
 };
 
 module.exports = ArmstrongNumber;
