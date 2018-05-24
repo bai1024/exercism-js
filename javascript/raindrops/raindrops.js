d2ce8c3b3d5d2213693deef29d0d644ed1f7193a
BaiYan <dingdingbai0424@gmail.com>
2018-05-24 09:35:35 +0800

    refactor code

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -1,28 +1,9 @@
 class Raindrops {
-  constructor() {}
-  convert(num){
-    let factors = Array
-      .from(Array(num), (_, i) => i)
-      .filter(i => num % i === 0)
-    factors.push(num)
-    
-    const result = factors.filter(i =>{
-      return i === 3 || i === 5 || i === 7
-    })
-    
-    let str = ''
-    
-    for(var i = 0; i < result.length;i++){
-      if(result[i] === 3){
-        str += 'Pling'
-      } else if(result[i] === 5){
-        str += 'Plang'
-      } else if (result[i] === 7){
-        str += 'Plong'
-      }
-    }
-    
-    return result.length === 0 ? num.toString() : str
+  convert(n) {
+    let output = '';
+    let map = new Map([[3, 'Pling'], [5, 'Plang'], [7, 'Plong']]);
+    map.forEach((word, k) => {output += !(n % k) ? word : ''});
+    return output || `${n}`;
   }
 }
 
