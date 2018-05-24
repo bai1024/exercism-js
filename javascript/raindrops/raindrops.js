f7481f8491b2b2eeb11e671009b0228f234d0d03
BaiYan <dingdingbai0424@gmail.com>
2018-05-24 09:36:29 +0800

    refactor code

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -8,11 +8,3 @@ class Raindrops {
 }
 
 module.exports = Raindrops
-
-//better solution
-// convert(n) {
-//   let output = '';
-//   let map = new Map([[3, 'Pling'], [5, 'Plang'], [7, 'Plong']]);
-//   map.forEach((word, k) => {output += !(n % k) ? word : ''});
-//   return output || `${n}`;
-// }
